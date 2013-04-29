d18e48129847202b340f5162dd9ac828e7ad2447
yueinteractive <yueinteractive@gmail.com>
2013-04-29 13:46:45 -0400

    Hide other panel when clicking

--- js/panel.js
+++ js/panel.js
@@ -16,11 +16,16 @@ panels.each(function(panelIndex, panel) {
 			var hasExpanded = $(panelContent).hasClass('expanded');
 			panelContents.removeClass('expanded');
 
+
 			if(!hasExpanded) {
 				$(panelContent).addClass('expanded');
 				panelOffsetX = - 400 * buttonIndex;
+				$(this).parent().prevAll().css('visibility', 'hidden');
+				$(this).parent().next().nextAll().css('visibility', 'hidden');
 			}else{
 				panelOffsetX = 0;
+				$(this).parent().prevAll().css('visibility', 'visible');
+				$(this).parent().next().nextAll().css('visibility', 'visible');
 			}
 
 			$(panelContainer).css('left', panelOffsetX + 'px');
