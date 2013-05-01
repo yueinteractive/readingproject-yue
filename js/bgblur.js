f38ecc9838153da3ad104cea1ca6ac31f88b86fd
yueinteractive <yueinteractive@gmail.com>
2013-04-30 20:37:59 -0400

    I create a blurb demo, but when apply to my project, it doesn't work well

---
+++ js/bgblur.js
@@ -0,0 +1,14 @@
+  $("#styled").on('mouseenter',function(){
+    $('#home').addClass("enterTextArea",1000);
+  });
+  $("#styled").on('mouseleave',function(){
+    $('#home').removeClass("enterTextArea","slow");
+  });
+
+
+  $("#styled2").on('mouseenter',function(){
+    $('#feedback').addClass("enterTextArea",1000);
+  });
+  $("#styled2").on('mouseleave',function(){
+    $('#feedback').removeClass("enterTextArea","slow");
+  });
